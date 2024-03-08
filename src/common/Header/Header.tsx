6c17d3ae4f4c0b3e0896077eb44065f1e7cab70e
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-09 01:00:12 +0400

    Updated

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -50,8 +50,8 @@ const Header: React.FC = () => {
                                 </button>
                             </li>
                             <select className={`${styles.select} font-poppions-light`} name="lang" id="lang">
-                                <option value="EN">EN</option>
-                                <option value="AZ">AZ</option>
+                                <option value="en">🇺🇸 En</option>
+                                <option value="az">🇦🇿 Az</option>
                             </select>
                         </ul>
 
--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -54,8 +54,8 @@ const MobileMenu: React.FC<ToogleProps> = ({ toggle, isToggled }) => {
                     </li>
                     <li>
                         <select className={`${styles.select} font-poppions-thin`} name="lang" id="lang">
-                            <option value="EN">EN</option>
-                            <option value="AZ">AZ</option>
+                            <option value="en">🇺🇸 En</option>
+                            <option value="az">🇦🇿 Az</option>
                         </select>
                     </li>
                 </ul>
