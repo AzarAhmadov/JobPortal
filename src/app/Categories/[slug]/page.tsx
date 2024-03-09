959c30b80d9aa366a8511af55d1d3f743183e798
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-09 15:05:31 +0400

    Updated

--- src/app/Categories/[slug]/page.tsx
+++ src/app/Categories/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import Heading from '@/common/Heading/Heading'
 import CatagoryFilter from '@/components/CatagoryFilter/CatagoryFilter'
-import styles from '../Categories.module.css'
+import styles from './Categories.module.css'
 
 const Page = () => {
     return (
