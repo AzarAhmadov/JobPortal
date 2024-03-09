e888d3eb525801170c06101b2dab3687be7c1497
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-09 14:21:27 +0400

    Updated

---
+++ src/app/Create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Create.module.css'
+
+const page: React.FC = () => {
+    return (
+        <section className={styles.create}>
+            Lorem.
+        </section>
+    )
+}
+
+export default page
--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -34,7 +34,7 @@ const Header: React.FC = () => {
                     <div>
                         <ul className={`${styles.list} font-poppions-light`}>
                             <li>
-                                <Link className={styles.create} href={''}>
+                                <Link className={styles.create} href={'/Create'}>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" /></svg>
                                     Create a vacancy
                                 </Link>
