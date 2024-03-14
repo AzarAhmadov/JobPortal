8a708cd3278716a3957816324061b52ee9c3cb20
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-15 02:33:49 +0400

    Updated

--- src/components/CatagoryFilter/CatagoryFilter.tsx
+++ src/components/CatagoryFilter/CatagoryFilter.tsx
@@ -46,7 +46,15 @@ const CatagoryFilter: React.FC = () => {
                             <span className={styles.circle}></span>
                             <li>{el.detail_jobs.map((detail) => detail.location)}</li>
                         </ul>
-                        <span className={`${styles.salary} font-poppions-medium`}>{el?.salary}</span>
+                        {el?.salary && el?.salary.length > 0 ? (
+                            <span className={`${styles.salary} font-poppions-medium`}>
+                                {el.salary}
+                            </span>
+                        ) : (
+                            <span className={`${styles.salary} font-poppions-medium`}>
+                                {t('salaryText')}
+                            </span>
+                        )}
                         <p className='font-poppions-thin'>{el?.desc}</p>
                         <span className={`${styles.date} font-poppions-thin`}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" /></svg>
