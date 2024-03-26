8cf6f1c0d76de0e2a6eba0b508a0bdf9e080a5fa
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-26 17:32:23 +0400

    Updated

--- src/app/[locale]/admin/dashboard_admin_panel_01/postDetail/[...slug]/page.tsx
+++ src/app/[locale]/admin/dashboard_admin_panel_01/postDetail/[...slug]/page.tsx
@@ -3,15 +3,6 @@ import styles from './postDetail.module.css';
 import { getCategory, getLand } from '@/lib/utils/renderFunction';
 import { GetVacancy } from '@/lib/data/data';
 import { getScopedI18n } from '@/locales/server';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    robots: {
-        index: false,
-        nocache: true,
-    },
-};
-
 export interface PageParams {
     params: {
         slug: string;
@@ -40,7 +31,7 @@ const Page: React.FC<PageParams> = async ({ params }) => {
         <>
             <div className='container'>
                 <div className={styles.apply}>
-                    <img src={vacancy?.company_logo} alt={vacancy?.company_name} />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOiDvxNABzHeNJGMEAfUm-tMbEMzwuXrIa6FNdIlBD8w&s' alt={vacancy?.company_name} />
                     <div className={styles.row}>
                         <div className={styles.top}>
                             <h4 className={`${styles.job} font-poppions-medium`}>{vacancy?.job_title}</h4>
@@ -59,27 +50,18 @@ const Page: React.FC<PageParams> = async ({ params }) => {
                             </span>
                         </div>
                         <div className={styles.rightArea}>
-                            <svg className={styles.bookMark} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553-6-3.428-6 3.428V4h12v14.553z"></path></svg>
                             <a target='_blank' className='font-poppions-light'
                                 href={ToLink}
-                            >{t('apply')}
+                            >
+                                {t('apply')}
+
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                     <path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z" /></svg>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div className={`${styles.src} font-poppions-light`}>
-                    <span>
-                        {vacancy?.company_logo}
-                    </span>
-                    <span>
-                        {vacancy.apply_method_1}
-                    </span>
-                    <span>
-                        {vacancy.apply_method_2}
-                    </span>
-                </div>
+
                 <div className={styles.detail}>
                     <div className={styles.detailRow}>
                         <div className={styles.left}>
@@ -106,7 +88,7 @@ const Page: React.FC<PageParams> = async ({ params }) => {
                                 <li>
                                     <span className={`${styles.title} font-poppions-thin`}>{t('last_date')}</span>
                                     <span className={`${styles.desc} font-poppions-medium`}>{vacancy?.last_date}</span>
-                                </li >
+                                </li>
                                 <li>
                                     <span className={`${styles.title} font-poppions-thin`}>
                                         {t('JobType')}
@@ -127,7 +109,7 @@ const Page: React.FC<PageParams> = async ({ params }) => {
                                         </span>
                                     )}
                                 </li>
-                            </ul >
+                            </ul>
                             <div>
                                 <h2 className='font-poppions-medium'>
                                     {t('category')}
@@ -136,9 +118,9 @@ const Page: React.FC<PageParams> = async ({ params }) => {
                                     {vacancy && getCategory(vacancy.category, t)}
                                 </button>
                             </div>
-                        </div >
-                    </div >
-                </div >
+                        </div>
+                    </div>
+                </div>
             </div >
         </>
     );
