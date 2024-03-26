547848c631b5d562983b5948289ef15609e7136f
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-26 21:16:06 +0400

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/assets/css/globals.css'
 import I18nProviderClientProvider from "@/components/I18nProviderClientProvider/I18nProviderClientProvider";
 import { Metadata } from "next";
-import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
     metadataBase: new URL("http://jobhubcenter.vercel.app/az"),
@@ -23,19 +23,7 @@ export default function RootLayout({
                 <link rel="icon" type="image/x-icon" href="/images/logo.png" />
                 <link rel="author" href="https://azarahmadov.com" />
                 <meta name="author" content="Job Hub Center LLC" />
-                <meta name="keywords" content="iş,iş elanları,iş tapmaq,vakansiyalar,vakansiya,azərbaycan vakansiya saytı,məşğulluq,keryera,iş axtarma,iş müraciəti,iş tapmaq platformu,işlər,jobhubcenter,Azar Ahmadov,azarahmadov,job board,bakıda işlər,iş elanları,elanlar"></meta>
-                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JWQTSGCDYV"></Script>
-                <Script id='google-analytics'>
-                    {
-                        `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                      
-                        gtag('config', 'G-JWQTSGCDYV');
-                    `
-                    }
-                </Script>
+                <meta name="keywords" content="iş,iş elanları,iş tapmaq,vakansiyalar,vakansiya,azərbaycan vakansiya saytı,məşğulluq,keryera,iş axtarma,iş müraciəti,iş tapmaq platformu,işlər,jobhubcenter,Azar Ahmadov,azarahmadov,job board,bakıda işlər,iş elanları,elanlar" />
             </head>
             <body className='body'>
                 <I18nProviderClientProvider>
@@ -45,6 +33,7 @@ export default function RootLayout({
                         </div>
                     </div>
                 </I18nProviderClientProvider>
+                <Analytics />
             </body>
         </html>
     );
