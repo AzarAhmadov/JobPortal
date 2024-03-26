3a0a7b49dde3bcff3293e212ecfed367b9ed9904
AzarAhmadov <ehmedovh111@gmail.com>
2024-03-26 21:12:22 +0400

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,21 +24,18 @@ export default function RootLayout({
                 <link rel="author" href="https://azarahmadov.com" />
                 <meta name="author" content="Job Hub Center LLC" />
                 <meta name="keywords" content="iş,iş elanları,iş tapmaq,vakansiyalar,vakansiya,azərbaycan vakansiya saytı,məşğulluq,keryera,iş axtarma,iş müraciəti,iş tapmaq platformu,işlər,jobhubcenter,Azar Ahmadov,azarahmadov,job board,bakıda işlər,iş elanları,elanlar"></meta>
-
                 <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JWQTSGCDYV"></Script>
-
                 <Script id='google-analytics'>
                     {
                         `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'G-JWQTSGCDYV');
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                      
+                        gtag('config', 'G-JWQTSGCDYV');
                     `
                     }
                 </Script>
-
             </head>
             <body className='body'>
                 <I18nProviderClientProvider>
