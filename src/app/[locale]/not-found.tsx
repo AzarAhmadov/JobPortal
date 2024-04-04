38aca61f070dffa98adbae2e8f07441a5839f073
AzarAhmadov <ehmedovh111@gmail.com>
2024-04-04 12:17:23 +0400

    Updated

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound: React.FC = () => {
     return (
         <section className={styles.error}>
             <div className={styles.img}>
-                <Image src="/images/notFound.png" alt="not-found" fill />
+                <Image priority src="/images/notFound.png" alt="not-found" fill />
             </div>
             <h3 className="font-poppions-light">Page Not Found</h3>
 
