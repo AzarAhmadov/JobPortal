6c5c74c6b8518e8311b826a0f3f1868fca0f9c12
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-16 15:53:59 +0400

    Updated Dashboard link

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -155,7 +155,7 @@ const DashBoard: React.FC<any> = async ({ start, end }) => {
             </div>
 
             <div className={styles.create}>
-                <Link className='font-poppions-light' href={'dashboard_admin_panel_01/adminPost'}>
+                <Link className='font-poppions-light' href={'/admin/dashboard/adminPost'}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" /></svg>
                 </Link>
             </div>
