6eb8bd1b566e200261a86d1c537b4e79d4b74dda
AzarAhmadov <ehmedovh111@gmail.com>
2024-06-17 11:32:03 +0400

    Update page.tsx

--- src/app/[locale]/admin/dashboard/page.tsx
+++ src/app/[locale]/admin/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import DashBoard from '@/components/DashBoard/DashBoard'
 import { PagePropsParams } from '@/types/Types';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 import React from 'react'
 
 export const metadata: Metadata = {
@@ -10,7 +12,20 @@ export const metadata: Metadata = {
     },
 };
 
+const getToken = () => {
+    const token = cookies().get('token');
+
+    return token;
+};
+
+
 const Page: React.FC<PagePropsParams> = async ({ searchParams }) => {
+
+    const token = getToken();
+
+    if (!token) {
+        redirect('/admin')
+    }
 
     const page = searchParams && 'page' in searchParams ? searchParams['page'] : '1';
     const per_page = searchParams && 'per_page' in searchParams ? searchParams['per_page'] : '10';
